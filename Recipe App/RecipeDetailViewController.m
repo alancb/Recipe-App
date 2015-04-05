713cf1de6e4aee5fc7477f26db74d2ee08962e78
Alan Barth <alancbarth@gmail.com>
2015-04-05 13:47:36 -0600

    Fixes a few views.

--- Recipe App/RecipeDetailViewController.m
+++ Recipe App/RecipeDetailViewController.m
@@ -7,6 +7,10 @@
 //
 
 #import "RecipeDetailViewController.h"
+#import "RARecipes.h"
+#import "RecipesTableVIewDataSource.h"
+
+static CGFloat margin = 20;
 
 @interface RecipeDetailViewController ()
 
@@ -16,7 +20,53 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    self.view.backgroundColor = [UIColor greenColor];
+    self.title = [RARecipes titleAtIndex:self.indexForRecipe];
+    UIScrollView *scrollView = [[UIScrollView alloc] initWithFrame:self.view.bounds];
+    [self.view addSubview:scrollView];
+    
+    //CGFloat heightForDescription = [self heightForDescription:[RARecipes descriptionAtIndex:self.index]];
+    
+    UILabel *description = [[UILabel alloc] initWithFrame:CGRectMake(margin, margin, self.view.frame.size.width - 2 * margin, 50)];
+    //Need to fix height to adjust to how long the actual description is.
+    description.text = [RARecipes descriptionAtIndex:self.indexForRecipe];
+    description.numberOfLines = 0;
+    [scrollView addSubview:description];
+    
+    UILabel *ingredientsTitle  = [[UILabel alloc] initWithFrame:CGRectMake(margin, margin + 100, self.view.frame.size.width - 2 * margin, 20)];
+    // Need to adjust height to fit for description
+    ingredientsTitle.text = @"Ingredients";
+    [scrollView addSubview:ingredientsTitle];
+    
+    CGFloat myNewY = margin + 20;
+    
+    for (int i = 0; i < [RARecipes ingredientCountAtIndex:self.indexForRecipe]; i++) {
+        UILabel *volume = [[UILabel alloc] initWithFrame:CGRectMake(margin, myNewY + 200, self.view.frame.size.width - 2 * margin, 40)];
+        volume.text = [RARecipes ingredientVolumeAtIndex:i inRecipeAtIndex:self.indexForRecipe];
+        [scrollView addSubview:volume];
+        
+        UILabel *type = [[UILabel alloc] initWithFrame:CGRectMake(margin, myNewY, self.view.frame.size.width - 2 * margin, 40)]; // Fix that! Boooo
+        type.text = [RARecipes ingredientTypeAtIndex:i inRecipeAtIndex:self.indexForRecipe];
+        [scrollView addSubview:type];
+        myNewY += (20 + margin);
+    }
+    
+    UILabel *directionsTitle = [[UILabel alloc] initWithFrame:CGRectMake(margin, 20, 20, 20)]; // Fix this
+    directionsTitle.text = @"Directions";
+    directionsTitle.font = [UIFont boldSystemFontOfSize:17];
+    [scrollView addSubview:directionsTitle];
+    
+    for (int i = 0; i < [[RARecipes directionsAtIndex:self.indexForRecipe] count] ; i++) {
+        UILabel * count = [[UILabel alloc] initWithFrame:CGRectMake(11, 1, 1, 1)];
+        count.text = [NSString stringWithFormat:@"%d", i + 1 ];
+        [scrollView addSubview:count];
+        
+        
+        UILabel * directions = [[UILabel alloc] initWithFrame:CGRectMake(1, 900, 1, 1)]; //fix it
+        directions.text = [RARecipes directionsAtIndex:self.indexForRecipe][i];
+        [scrollView addSubview:directions];
+    }
+    scrollView.contentSize = CGSizeMake(self.view.frame.size.width, 600); //change height to be equal to something better
 }
 
 - (void)didReceiveMemoryWarning {
